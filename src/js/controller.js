b252d4d014c2695066b5827a15dbcdb9ba7b5bd2
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2022-08-25 13:55:03 +0700

    render the menu

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,9 +2,11 @@ import '../sass/main.scss';
 
 import * as model from './model';
 import homepageView from './homepageView';
+import menuView from './menuView';
 
 function init() {
-  homepageView.render();
+  // homepageView.render();
+  menuView.render();
 }
 
-// init();
+init();
--- src/js/menuView.js
+++ src/js/menuView.js
@@ -1,5 +1,18 @@
 import View from './View';
 
 class menuView extends View {
-  generateMarkup() {}
+  _generateMarkup() {
+    return `
+      <div class="menu__interface">
+        <ul class="menu__navigation-bar">
+          <li class="menu__category">By Name</li>
+          <li class="menu__category">By Flavor</li>
+          <li class="menu__category">By Type</li>
+        </ul>
+        <div class="menu__drinks-display"></div>
+      </div>
+    `;
+  }
 }
+
+export default new menuView();
